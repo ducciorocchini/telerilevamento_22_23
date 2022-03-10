3ee98a0088ac57c20eae30871ebb5c234d0901ff
ducciorocchini <ducciorocchini@gmail.com>
2022-03-10 18:04:43 +0100

    Update R_code_rs.r

--- R_code_rs.r
+++ R_code_rs.r
@@ -1,3 +1,26 @@
 # Questo è il primo script che useremo a lezione
 
+# install.packages("raster")
 library(raster)
+
+# Settaggio cartella di lavoro
+setwd("~/lab/") # Linux
+# setwd("/Users/emma/desktop/lab") #mac
+# setwd("C:/lab/") # windows
+
+# import
+l2011 <- brick("p224r63_2011.grd")
+l2011
+
+# plot
+plot(l2011)
+
+# https://www.r-graph-gallery.com/42-colors-names.html
+cl <- colorRampPalette(c("black", "grey", "light grey")) (100)
+plot(l2011, col=cl)
+
+# dev.off()
+
+
+
+
