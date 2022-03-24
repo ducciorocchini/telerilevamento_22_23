7bf0670f38a1dc0c1d67ba8f8cf4d5c94aefddd1
ducciorocchini <ducciorocchini@gmail.com>
2022-03-24 17:33:57 +0100

    Update R_code_rs.r

--- R_code_rs.r
+++ R_code_rs.r
@@ -84,14 +84,41 @@ plot(l2011$B3_sre, col=clr)
 clnir <- colorRampPalette(c("red", "orange", "yellow")) (100)
 plot(l2011$B4_sre, col=clnir)
 
+# Day #3
 
+# Plot of l2011 in the NIR channel (NIR band)
+clnir <- colorRampPalette(c("red", "orange", "yellow")) (100)
+plot(l2011$B4_sre, col=clnir)
+# or:
+plot(l2011[[4]])
 
+# Landsat ETM+
+# b1 = blu
+# b2 = verde
+# b3 = rosso
+# b4 = infrarosso vicino NIR
 
+# plot RGB layers
+plotRGB(l2011, r=3, g=2, b=1, stretch="lin")
+plotRGB(l2011, r=3, g=4, b=2, stretch="lin")
+plotRGB(l2011, r=3, g=2, b=4, stretch="lin")
 
+plotRGB(l2011, r=3, g=4, b=2, stretch="hist")
 
+# Exercise: build a multiframe with visible RGB 
+# (linear stretch) on top of false colours
+# (histogram stretch) 
+par(mfrow=c(2,1))
+plotRGB(l2011, r=3, g=2, b=1, stretch="lin")
+plotRGB(l2011, r=3, g=4, b=2, stretch="hist")
 
+# Exercise: upload the image from 1988
+l1988 <- brick("p224r63_1988.grd")
+l1988
 
-
+par(mfrow=c(2,1))
+plotRGB(l1988, r=4, g=3, b=2, stretch="lin")
+plotRGB(l2011, r=4, g=3, b=2, stretch="lin")
 
 
 
