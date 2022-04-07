17fb170dca6c67115719b6df6cdf46b4428a67ac
ducciorocchini <ducciorocchini@gmail.com>
2022-04-07 17:47:04 +0200

    Update R_code_time_series_greenland.r

--- R_code_time_series_greenland.r
+++ R_code_time_series_greenland.r
@@ -71,5 +71,29 @@ plot(en13, col=cl)
 # Exercise: import the whole as in the Greenland example
 # by the following steps: list.files, lapply, stack 
 
+rlist <- list.files(pattern="EN")
+
+# lapply(X,FUN)
+rimp <- lapply(rlist, raster)
+
+# stack
+en <- stack(rimp)
+
+# plot everything
+plot(en, col=cl)
+
+# Exercise: plot EN01 besides EN13
+par(mfrow=c(1,2))
+plot(en[[1]], col=cl)
+plot(en[[13]], col=cl)
+
+# or:
+en113 <- stack(en[[1]], en[[13]])
+plot(en113, col=cl)
+
+# let's make the difference:
+difen <-  en[[1]] - en[[13]]
+cldif <- colorRampPalette(c('blue','white','red'))(100) # 
+plot(difen, col=cldif)
 
 
