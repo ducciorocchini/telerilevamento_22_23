76fd394824e8e5ea451f0030986096a4c8a4515c
ducciorocchini <ducciorocchini@gmail.com>
2022-04-29 17:45:53 +0200

    Update R_code_variability.r

--- R_code_variability.r
+++ R_code_variability.r
@@ -4,6 +4,7 @@ library(raster)
 library(RStoolbox) # for image viewing and variability calculation
 library(ggplot2) # for ggplot plotting
 library(patchwork)
+library(viridis)
 
 setwd("~/lab/") # Linux
 # setwd("C:/lab/") # Windows
@@ -30,5 +31,38 @@ g1+g2
 # calculation of variability over NIR
 nir <- sen[[1]]
 
-sd <- focal(nir, matrix(1/9, 3, 3), fun=sd)
+sd3 <- focal(nir, matrix(1/9, 3, 3), fun=sd)
+
+clsd <- colorRampPalette(c('blue','green','pink','magenta','orange','brown','red','yellow'))(100) # 
+plot(sd3, col=clsd)
+
+# plotting with ggplot
+ggplot() + 
+geom_raster(sd3, mapping =aes(x=x, y=y, fill=layer)) 
+
+# with viridis
+ggplot() + 
+geom_raster(sd3, mapping =aes(x=x, y=y, fill=layer)) + 
+scale_fill_viridis() +
+ggtitle("Standard deviation by viridis package")
+
+# cividis
+ggplot() + 
+geom_raster(sd3, mapping =aes(x=x, y=y, fill=layer)) + 
+scale_fill_viridis(option = "cividis") +
+ggtitle("Standard deviation by viridis package")
+
+# magma
+ggplot() + 
+geom_raster(sd3, mapping =aes(x=x, y=y, fill=layer)) + 
+scale_fill_viridis(option = "magma") +
+ggtitle("Standard deviation by viridis package")
+
+# Exercise: make the same calculation with a 7x7 window
+sd7 <- focal(nir, w=matrix(1/49, 7, 7), fun=sd)
+
+
+
+
+
 
